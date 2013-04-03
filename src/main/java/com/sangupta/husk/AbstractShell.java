bb0c2858ad6d64ed31d5457a7ac9a2b96d85d8e4
Sandeep Gupta <sandy.pec@gmail.com>
2013-04-03 14:04:32 +0530

    Added documentation

--- src/main/java/com/sangupta/husk/AbstractShell.java
+++ src/main/java/com/sangupta/husk/AbstractShell.java
@@ -22,6 +22,7 @@
 package com.sangupta.husk;
 
 import com.sangupta.husk.core.HuskShellContext;
+import com.sangupta.husk.core.HuskShellContextAware;
 import com.sangupta.husk.core.PromptProvider;
 
 /**
@@ -34,26 +35,63 @@ import com.sangupta.husk.core.PromptProvider;
  */
 public abstract class AbstractShell implements IShell {
 	
+	/**
+	 * The prompt that needs to be displayed
+	 */
 	protected String prompt;
 	
+	/**
+	 * The prompt provider to use, if any. If a prompt provider is provided
+	 * then it used, else, the given prompt is shown.
+	 * 
+	 */
 	protected PromptProvider promptProvider;
 	
+	/**
+	 * Character to use to mask screen when reading passwords.
+	 * 
+	 */
 	protected char maskCharacter;
 	
+	/**
+	 * Names of commands to use to exit the shell. The array is initialized
+	 * to the default name consisting of <strong>exit</strong>.
+	 * 
+	 */
 	protected String[] exitCommandNames = { "exit" };
 	
+	/**
+	 * Names of commands to use to print short help for the shell. The array 
+	 * is initialized to the default name consisting of <strong>help</strong>.
+	 */
 	protected String[] helpCommandNames = { "help" };
 	
+	/**
+	 * Shell context to use to be passed to various commands that implement
+	 * the {@link HuskShellContextAware} interface.
+	 */
 	protected HuskShellContext shellContext;
 	
+	/**
+	 * Set the new value of the prompt.
+	 * 
+	 */
 	public void setPrompt(String prompt) {
 		this.prompt = prompt;
 	}
 
+	/**
+	 * Set the new masking character.
+	 * 
+	 */
 	public void setMaskCharacter(char mask) {
 		this.maskCharacter = mask;
 	}
 	
+	/**
+	 * Set a new exit command name, ignoring previous names.
+	 * 
+	 */
 	public void setExitCommandNames(String exitName) {
 		if(exitName == null) {
 			return;
@@ -62,6 +100,10 @@ public abstract class AbstractShell implements IShell {
 		this.exitCommandNames = new String[] { exitName };
 	}
 
+	/**
+	 * Set new exit command names, ignoring previous ones.
+	 * 
+	 */
 	public void setExitCommandNames(String[] exitNames) {
 		if(exitNames == null) {
 			return;
@@ -70,6 +112,10 @@ public abstract class AbstractShell implements IShell {
 		this.exitCommandNames = exitNames;
 	}
 	
+	/**
+	 * Set new help command names, ignoring previous ones.
+	 * 
+	 */
 	public void setHelpCommandNames(String helpName) {
 		if(helpName == null) {
 			return;
@@ -78,6 +124,10 @@ public abstract class AbstractShell implements IShell {
 		this.helpCommandNames = new String[] { helpName };
 	}
 
+	/**
+	 * Set new help command names, ignoring previous ones.
+	 * 
+	 */
 	public void setHelpCommandNames(String[] helpNames) {
 		if(helpNames == null) {
 			return;
@@ -86,10 +136,19 @@ public abstract class AbstractShell implements IShell {
 		this.helpCommandNames = helpNames;
 	}
 
+	/**
+	 * Set a new {@link PromptProvider}.
+	 * 
+	 * @param promptProvider
+	 */
 	public void setPromptProvider(PromptProvider promptProvider) {
 		this.promptProvider = promptProvider;
 	}
 	
+	/**
+	 * Set a new {@link HuskShellContext}.
+	 * 
+	 */
 	@Override
 	public void setHuskShellContext(HuskShellContext shellContext) {
 		this.shellContext = shellContext;
